d492a9f9458c2d9375ba7e40a4932cbdf4a46e99
Valentin Kunz <kunzvalentin4@gmail.com>
2025-02-06 22:39:07 +0100

    Created the models Tip and Comment for the tips section

--- tips/models.py
+++ tips/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Tip(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(blank=False, null=False, max_length=200)
+    body = models.TextField(blank=False, null=False)
+    likes = models.PositiveIntegerField()
+    img = models.ImageField(upload_to="uploads/")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ["likes"]
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    tip = models.ForeignKey(
+        Tip,
+        on_delete=models.CASCADE
+    )
+    body = models.TextField(blank=False, null=False)
+    likes = models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
+    
+    class Meta:
+        ordering = ["likes"]
--- tips/views.py
+++ tips/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+from .models import *
+
+
+class TipListView(ListView):
+    model = Tip
+    template_name = "tips/tips.html"
