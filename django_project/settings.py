c02c64d73bded28d3aa44a78da439216e6571306
Valentin Kunz <kunzvalentin4@gmail.com>
2025-02-13 18:28:54 +0100

    Updated ALLOWED_HOSTS

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["d8l-assiduous-ohm.circumeo-apps.net"]
 
 
 # Application definition
