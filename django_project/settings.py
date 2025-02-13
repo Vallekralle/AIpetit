0cc801b627a99811d57ff29747a7660360452c5c
Valentin Kunz <kunzvalentin4@gmail.com>
2025-02-13 22:54:08 +0100

    Showing likes now

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"]
 
-ALLOWED_HOSTS = ["d8l-assiduous-ohm.circumeo-apps.net"]
+ALLOWED_HOSTS = ["d8l-assiduous-ohm.circumeo-apps.net", "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = ["https://d8l-assiduous-ohm.circumeo-apps.net"]
 
